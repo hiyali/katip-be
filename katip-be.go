c7c7406b5beb7cd4148227977fa000d418e05358
Salam Hiyali <salam@liepiaowang.com>
2018-05-21 15:04:41 +0800

    Move codes

--- katip-be.go
+++ katip-be.go
@@ -10,6 +10,11 @@ func main() {
 
   e.Use(middleware.Logger())
   e.Use(middleware.Recover())
+  e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+    Skipper:      middleware.DefaultSkipper,
+    AllowOrigins: []string{"*"},
+    AllowMethods: []string{echo.GET, echo.HEAD, echo.PUT, echo.PATCH, echo.POST, echo.DELETE, echo.OPTIONS},
+  }))
 
   e.Validator = &Validator{}
 
